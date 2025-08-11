64afa93259049f0cd16b9eff438f31cefb63949f
bytesparrow <5909549+bytesparrow@users.noreply.github.com>
2025-08-11 16:58:37 +0200

    code reformat

--- classes/observer.php
+++ classes/observer.php
@@ -1,37 +1,41 @@
 <?php
+
 namespace mod_messagestream;
 
 defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/mod/messagestream/lib.php');
 
 class observer {
-    public static function handle_nmstream_entry_written(\local_nmstream\event\comment_created $event) {
-        $userid = $event->userid;
-        $messagestreamid = $event->objectid;
-       
-        $contextid = $event->contextinstanceid;
-        $courseid = $event->courseid;
 
-        $timecreated = $event->timecreated;
-        \messagestream_set_points_for_user($contextid, $userid);
-        // Do something – for example, log or update something
-       //debugging("local_nmstream received stream_entry_written event for user $userid and stream $messagestreamid in Context (cmid) $contextid in course $courseid at  $timecreated", DEBUG_DEVELOPER);
-    }
-    public static function handle_nmstream_entry_deleted(\local_nmstream\event\comment_deleted $event) {
-        $userid = $event->userid;
-        $messagestreamid = $event->objectid;
-       
-        #$contextid = $event->context;
-        $contextid = $event->other["rootid"]  ;
-        $courseid = $event->courseid;
+  public static function handle_nmstream_entry_written(\local_nmstream\event\comment_created $event) {
+    $userid = $event->userid;
+    $messagestreamid = $event->objectid;
 
-        $timecreated = $event->timecreated;
-        
-        return messagestream_set_points_for_user($contextid, $userid, true);
-      #  return;
-      #  \messagestream_award_points_for_user($contextid, $userid);
-        // Do something – for example, log or update something
-     #  var_dump("local_nmstream received comment_deleted event for user $userid and stream $messagestreamid in Context (cmid) $contextid in course $courseid at  $timecreated", DEBUG_DEVELOPER);
-      # exit;
-    }
+    $contextid = $event->contextinstanceid;
+    $courseid = $event->courseid;
+
+    $timecreated = $event->timecreated;
+    \messagestream_set_points_for_user($contextid, $userid);
+    // Do something – for example, log or update something
+    //debugging("local_nmstream received stream_entry_written event for user $userid and stream $messagestreamid in Context (cmid) $contextid in course $courseid at  $timecreated", DEBUG_DEVELOPER);
+  }
+
+  public static function handle_nmstream_entry_deleted(\local_nmstream\event\comment_deleted $event) {
+    $userid = $event->userid;
+    $messagestreamid = $event->objectid;
+
+    #$contextid = $event->context;
+    $contextid = $event->other["rootid"];
+    $courseid = $event->courseid;
+
+    $timecreated = $event->timecreated;
+
+    return messagestream_set_points_for_user($contextid, $userid, true);
+    #  return;
+    #  \messagestream_award_points_for_user($contextid, $userid);
+    // Do something – for example, log or update something
+    #  var_dump("local_nmstream received comment_deleted event for user $userid and stream $messagestreamid in Context (cmid) $contextid in course $courseid at  $timecreated", DEBUG_DEVELOPER);
+    # exit;
+  }
+
 }
